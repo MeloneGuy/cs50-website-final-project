d302963693fa3ea641436463f90e64e270fd7790
meloneguy <timplatteschorre@gmail.com>
2023-11-05 17:16:50 +0100

    Updated JS script to work for all images

--- shared/js/norightclickimage.js
+++ shared/js/norightclickimage.js
@@ -1,7 +1,9 @@
-// Get the image element by its ID
-const pandaImage = document.getElementById("pandaImage");
+// Get all image elements with IDs containing "image"
+const imageElements = document.querySelectorAll('[id*="image"]');
 
-// Add an event listener to prevent the context menu
-pandaImage.addEventListener("contextmenu", function (e) {
-    e.preventDefault();
+// Add an event listener to prevent the context menu for each image
+imageElements.forEach((imageElement) => {
+    imageElement.addEventListener("contextmenu", function (e) {
+        e.preventDefault();
+    });
 });
